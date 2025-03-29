acb91fe2af1bf4f36220c831be3f018181a1cdf8
Igor Almeida <atila.sos@gmail.com>
2025-03-29 15:50:30 +0000

    Initial commit on develop branch

--- infantinho/settings.py
+++ infantinho/settings.py
@@ -167,7 +167,12 @@ LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/microsoft/to-auth-redirect/'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Show emails in console during development
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    # SMTP settings below are only used when DEBUG is False
+
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
